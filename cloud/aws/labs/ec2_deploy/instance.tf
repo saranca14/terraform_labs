ad3f53f93d2abf4feb3bf8059ee8eee18bc3da30
saranca14 <61488445+saranca14@users.noreply.github.com>
2023-07-05 22:38:25 +0200

    updated ec2_name

--- cloud/aws/labs/ec2_deploy/instance.tf
+++ cloud/aws/labs/ec2_deploy/instance.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "this" {
   ami           = "ami-038056f5d3df2259d"
   instance_type = var.instance_type
-  tags = {
+  tags          = {
     name = "my_ec2"
+    env  = "test"
   }
 }
